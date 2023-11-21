652e722f26e0bcec3f72c210a23f503087b3b651
edinson rosales <edidy9v@gmail.com>
2023-11-21 11:10:07 -0500

    VehicleDetails class is created

---
+++ WashingCarDB/DAL/Entities/VehicleDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WashingCarDB.DAL.Entities
+{
+    public class VehicleDetails : Entity
+    {
+
+        [Display(Name = "Vehiculo.")]
+        public virtual Vehicle Vehicle { get; set; }
+
+        [Display(Name = "Fecha de entrega")]
+        public virtual DateTime? DeliveryDate { get; set; }
+
+
+    }
+}
