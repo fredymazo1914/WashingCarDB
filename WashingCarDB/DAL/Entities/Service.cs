493dec57f594255ac3c5b359ab6372370687bd79
edinson rosales <edidy9v@gmail.com>
2023-11-21 10:25:14 -0500

    the service class is created

---
+++ WashingCarDB/DAL/Entities/Service.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WashingCarDB.DAL.Entities
+{
+    public class Service
+    {
+
+
+        #region Properties
+        [Display(Name = "Servicio.")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Vehiculos")]
+        public Guid VehicleId { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; }
+        // public ICollection<Vehicle> Vehicles { get; set; }
+
+
+        #endregion
+
+    }
+}
